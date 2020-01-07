4882839f99b246d610f9373316bf51736e82c6f0
Tim Biehl <tbiehl22@gmail.com>
2020-01-06 20:37:11 -0700

    added computer letter choices to js page

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var computerLetterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var lettersGuessed = [];
+var computerGuesses = [];
+
+
+
